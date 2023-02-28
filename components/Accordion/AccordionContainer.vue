5fca8690e88d600b1e19829b17816684c66e252a
Jio Mamaoag <jiomamaoag@gmail.com>
2023-02-28 16:22:29 +0800

    create accordion component

---
+++ components/Accordion/AccordionContainer.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="accordion__container">
+        <div class="container">
+            <AccordionItem v-for="(d, idx) in data" :selected="idx === selected" :accordionItem="idx" :key="idx" :title="d.title" :content="d.content" @handleSelect="handleSelected"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import AccordionItem from './AccordionItem.vue';
+
+    export default {
+    name: "AccordionContainerComponent",
+    data() {
+        return {
+            selected: 0
+        };
+    },
+    props: {
+        data: {
+            type: Array,
+            required: false,
+        }
+    },
+    methods: {
+        handleSelected(index) {
+            this.selected = (index === this.selected) ? null : index;
+        }
+    },
+    components: { AccordionItem }
+}
+</script>
+
+<style>
+@media (min-width: 768px) {
+    .accordion__container {
+        display: none;
+    }
+}
+</style>
---
+++ components/Accordion/AccordionItem.vue
@@ -0,0 +1,76 @@
+<template>
+    <div :class="{'active': selected}" class="accordion__content_container">
+        <!-- Header -->
+        <div class="accordion__header py-2 px-4" @click="handleSelected(accordionItem)">
+            <h3>{{ title }}</h3>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="svg_icon">
+                <path v-if="!selected" stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                <path v-else stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15" />
+            </svg>
+
+        </div>
+        <!-- Body -->
+        <article v-if="selected" class="accordion__content py-2 px-4" v-html="content"></article>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AccordionItemComponent.vue',
+        props: {
+            title: {
+                type: String
+            },
+            content: {
+                type: String
+            },
+            selected: {
+                type: Boolean
+            },
+            accordionItem: {
+                type: Number
+            }
+        },
+        methods: {
+            handleSelected(idx) {
+                this.$emit('handleSelect', idx)
+            }
+        }
+    }
+</script>
+
+<style>
+.accordion__content_container {
+    background: #fff;
+}
+
+.svg_icon {
+    width: 2rem;
+    height: 2rem;
+}
+
+.accordion__header {
+    display: flex;
+    justify-items: center;
+    justify-content: space-between;
+    align-items: center;
+    align-self: center;
+}
+
+.accordion__header:hover {
+    cursor: pointer;
+}
+
+.accordion__header h3 {
+    font-weight: bold;
+    letter-spacing: 0.06rem;
+}
+
+.accordion__content {
+    transform: all cubic-bezier(0, 1, 0.5, 1) 0.5s;
+}
+
+.accordion__content_container.active {
+    background: #fea706;
+}
+</style>
